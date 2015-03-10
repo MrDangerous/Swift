b0761da17f0ca3c1e91ac085dc562cb14e353226
xiewenqiang <xie_wen_qiang@sina.com>
2015-03-10 17:14:23 +0800

    Swift
    新建Swift工程

---
+++ SwiftDemo/SwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftDemo
+//
+//  Created by XWQ on 15/3/10.
+//  Copyright (c) 2015年 _Name.im_. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
